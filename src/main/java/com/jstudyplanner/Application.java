cb5a81d466bb8ed499edf189f16a3a30544da3f3
anithaedith <anithaedith@gmail.com>
2018-02-09 18:43:30 +0530

    modified:   src/main/java/com/jstudyplanner/Application.java 	modified:   src/main/java/com/jstudyplanner/web/CampusController.java 	modified:   src/main/resources/bootstrap.yml

--- src/main/java/com/jstudyplanner/Application.java
+++ src/main/java/com/jstudyplanner/Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication(exclude = {
@@ -18,6 +20,11 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate returnrestTemplate(){
+        return new RestTemplate();
     }
 
 
--- src/main/java/com/jstudyplanner/web/CampusController.java
+++ src/main/java/com/jstudyplanner/web/CampusController.java
@@ -3,24 +3,49 @@ package com.jstudyplanner.web;
 import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.support.PagedListHolder;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.jstudyplanner.domain.Campus;
 import com.jstudyplanner.service.CampusService;
 
+import java.util.List;
+
 @Controller
+@RefreshScope
 public class CampusController {
-	
+	Logger logger= LoggerFactory.getLogger(this.getClass());
+
 	private final CampusService campusService;
 	private final static int DEFAULT_PAGE_SIZE = 10;
-	
+
+	@Autowired
+	private RestTemplate restTemplate;
+	private String campusServiceUrl;
+
+	@Value("${campus.url}")
+	private String campus_url;
+
+	@Value("${campus.getall}")
+	private String campus_getall;
+
+
+
 	/**
 	 * CampusService implementation should be marked with @Component
 	 * @param campusService
@@ -36,13 +61,24 @@ public class CampusController {
 	 */
 	@RequestMapping(value = "/campuses/list", method = RequestMethod.GET)
 	public ModelAndView listCampuses(@RequestParam(required = false) Integer page, @RequestParam(required = false) Integer pageSize, HttpSession session) {
+		logger.info("inside listCampuses");
+		String uri=campus_url+campus_getall;
+		logger.info(uri);
+
 		if (page == null) {
 			page = 0;
 		}
 		// get enabled campuses instead of getAllCampuses
 		PagedListHolder<Campus> resultList = (PagedListHolder<Campus>) session.getAttribute("CampusController_resultList");
 		if (resultList == null) {
-			resultList = new PagedListHolder<Campus>(campusService.getAllCampuses());
+			ResponseEntity<List<Campus>> rateResponse =
+					restTemplate.exchange(uri,
+							HttpMethod.GET, null, new ParameterizedTypeReference<List<Campus>>() {
+							});
+			List<Campus> campuses = rateResponse.getBody();
+			logger.info("fetched campuses" +campuses.size());
+			//resultList = new PagedListHolder<Campus>(campusService.getAllCampuses());
+			resultList = new PagedListHolder<Campus>(campuses);
 			session.setAttribute("CampusController_resultList", resultList);
 			if (pageSize == null) {
 				pageSize = DEFAULT_PAGE_SIZE;
